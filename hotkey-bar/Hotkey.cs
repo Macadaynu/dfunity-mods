5d4e3678b021d35d3aa35361746e8627470ddb9b
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-05-15 10:58:16 +0100

    Create Hotkey.cs

---
+++ hotkey-bar/Hotkey.cs
@@ -0,0 +1,11 @@
+namespace Assets.Scripts.Game.MacadaynuMods.HotkeyBar
+{
+    public class Hotkey
+    {
+        public int Id { get; set; }
+
+        public HotkeyType Type { get; set; }
+
+        public string SpellName { get; set; }
+    }
+}
