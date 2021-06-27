d0e4b1d7d146af3713000403670d48ed773ddc01
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-06-27 15:43:57 +0100

    Create NearestQuest.cs

---
+++ quest-offer-locations/NearestQuest.cs
@@ -0,0 +1,11 @@
+using DaggerfallWorkshop.Game.Questing;
+
+namespace Assets.Scripts.Game.MacadaynuMods.QuestOfferLocations
+{
+    public class NearestQuest
+    {
+        public float TimeToTravelToQuestInDays { get; set; }
+
+        public Quest Quest { get; set; }
+    }
+}
