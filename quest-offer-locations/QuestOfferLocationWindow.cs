4ca4c4d9d732719cdb9451e304ad80d2b6229d16
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-06-27 15:42:25 +0100

    Update QuestOfferLocationWindow.cs

--- quest-offer-locations/QuestOfferLocationWindow.cs
+++ quest-offer-locations/QuestOfferLocationWindow.cs
@@ -1,14 +1,17 @@
-using UnityEngine;
 using DaggerfallWorkshop.Game.UserInterface;
 using DaggerfallWorkshop.Game.Questing;
 using DaggerfallConnect.Arena2;
 using DaggerfallWorkshop.Game.Entity;
 using Assets.Scripts.Game.MacadaynuMods.QuestOfferLocations;
+using DaggerfallConnect;
+using Assets.Scripts.Game.UserInterfaceWindows;
 
 namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 {
     public class QuestOfferLocationWindow : DaggerfallQuestOfferWindow
     {
+        public NearestQuest NearestQuest;
+
         #region Constructors
 
         public QuestOfferLocationWindow(IUserInterfaceManager uiManager, StaticNPC.NPCData npc, FactionFile.SocialGroups socialGroup, bool menu)
@@ -42,15 +45,75 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             int reputation = GameManager.Instance.PlayerEntity.FactionData.GetReputation(factionId);
             int level = GameManager.Instance.PlayerEntity.Level;
 
-            // Select a quest at random from appropriate pool
             offeredQuest = GameManager.Instance.QuestListsManager.GetSocialQuest(socialGroup, factionId, gender, reputation, level);
+
+            if (QuestOfferLocationsMod.preferNearbyQuests)
+            {
+                NearestQuest = null;
+
+                //int attempts = 0;
+                var timeStart = System.DateTime.Now;
+                bool found = false;
+                while (!found)
+                {
+                    // if close enough quest still not found after enough time has elapsed, stick with the current offered quest
+                    if (System.DateTime.Now.Subtract(timeStart).TotalSeconds >= QuestOfferLocationsMod.maxSearchTimeInSeconds)//++attempts >= 10)
+                    {
+                        found = true;
+                        offeredQuest = NearestQuest?.Quest;
+                        break;
+                    }
+
+                    if (offeredQuest == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var farthestTravelTimeInDays = 0.0f;
+                        var questPlaces = offeredQuest.GetAllResources(typeof(Place));
+                        foreach (Place questPlace in questPlaces)
+                        {
+                            DFLocation location;
+                            DaggerfallUnity.Instance.ContentReader.GetLocation(questPlace.SiteDetails.regionName, questPlace.SiteDetails.locationName, out location);
+
+                            var travelTimeDays = QuestOfferMessageHelper.GetTravelTimeToLocation(location);
+
+                            if (travelTimeDays > farthestTravelTimeInDays)
+                            {
+                                farthestTravelTimeInDays = travelTimeDays;
+                            }
+                        }
+
+                        if (!found && farthestTravelTimeInDays > QuestOfferLocationsMod.maxTravelDistanceInDays)
+                        {
+                            // store the nearest quest in the loop, as a fallback
+                            if (NearestQuest == null || NearestQuest.TimeToTravelToQuestInDays > farthestTravelTimeInDays)
+                            {
+                                NearestQuest = new NearestQuest
+                                {
+                                    Quest = offeredQuest,
+                                    TimeToTravelToQuestInDays = farthestTravelTimeInDays
+                                };
+                            }
+
+                            // Select a quest at random from appropriate pool
+                            offeredQuest = GameManager.Instance.QuestListsManager.GetSocialQuest(socialGroup, factionId, gender, reputation, level);
+                            continue;
+                        }
+                        else
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
             if (offeredQuest != null)
             {
-                // Log offered quest
-                Debug.LogFormat("Offering quest {0} from Social group {1} affecting factionId {2}", offeredQuest.QuestName, socialGroup, offeredQuest.FactionId);
-
                 // Offer the quest to player
-                var messageBox = QuestOfferMessageHelper.CreateQuestOffer(offeredQuest);
+                var messageBox = QuestOfferMessageHelper.CreateQuestOffer(offeredQuest); //QuestMachine.Instance.CreateMessagePrompt(offeredQuest, (int)QuestMachine.QuestMessages.QuestorOffer);// TODO - need to provide an mcp for macros?
                 if (messageBox != null)
                 {
                     messageBox.OnButtonClick += OfferQuest_OnButtonClick;
