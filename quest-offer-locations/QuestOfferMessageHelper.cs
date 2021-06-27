b539f0951532271f4f74665c7227b9d823627802
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-06-27 15:42:58 +0100

    Update QuestOfferMessageHelper.cs

--- quest-offer-locations/QuestOfferMessageHelper.cs
+++ quest-offer-locations/QuestOfferMessageHelper.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Game.UserInterfaceWindows;
 using DaggerfallConnect;
 using DaggerfallConnect.Arena2;
 using DaggerfallConnect.Utility;
@@ -7,6 +8,7 @@ using DaggerfallWorkshop.Game.Questing;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 using DaggerfallWorkshop.Game.Utility;
 using DaggerfallWorkshop.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -24,6 +26,10 @@ namespace Assets.Scripts.Game.MacadaynuMods.QuestOfferLocations
             Message acceptMessage = quest.GetMessage((int)QuestMachine.QuestMessages.AcceptQuest);
 
             var place = GetLastPlaceMentionedInMessage(acceptMessage);
+            if (place == null)
+            {
+                place = GetLastPlaceMentionedInMessage(message);
+            }
 
             if (place != null)
             {
@@ -84,6 +90,12 @@ namespace Assets.Scripts.Game.MacadaynuMods.QuestOfferLocations
         {
             QuestMacroHelper helper = new QuestMacroHelper();
             QuestResource[] resources = helper.GetMessageResources(message);
+
+            return GetLastPlaceInResources(resources);
+        }
+
+        public static Place GetLastPlaceInResources(QuestResource[] resources)
+        {
             if (resources == null || resources.Length == 0)
                 return null;
 
@@ -101,31 +113,117 @@ namespace Assets.Scripts.Game.MacadaynuMods.QuestOfferLocations
         {
             DFLocation location;
             DaggerfallUnity.Instance.ContentReader.GetLocation(place.SiteDetails.regionName, place.SiteDetails.locationName, out location);
+           
+            if (QuestOfferLocationsMod.useTravelOptionsTimeCalc)
+            {
+                return GetTravelTimeToLocationFromTravelOptions(location);
+            }
+            else
+            {
+                var travelTimeDays = GetTravelTimeToLocation(location);
+                return $"{(int)travelTimeDays} days travel";
+            }
+        }
+
+
+        static float ConvertMinutesToDays(int minutes)
+        {
+            TimeSpan result = TimeSpan.FromMinutes(minutes);
+
+            return (float)result.TotalDays;
+        }
+
+
+        public static float GetTravelTimeToLocation(DFLocation location)
+        {
             DFPosition position = MapsFile.LongitudeLatitudeToMapPixel(location.MapTableData.Longitude, location.MapTableData.Latitude);
 
-            bool hasHorse = GameManager.Instance.TransportManager.HasHorse();
-            bool hasCart = GameManager.Instance.TransportManager.HasCart();
+            if (QuestOfferLocationsMod.useTravelOptionsTimeCalc)
+            {
+                TransportManager transportManager = GameManager.Instance.TransportManager;
+                bool horse = transportManager.TransportMode == TransportModes.Horse;
+                bool cart = transportManager.TransportMode == TransportModes.Cart;
+
+                var travelTimeCalculator = new TravelTimeCalculator();
+
+                var travelTimeTotalMins = travelTimeCalculator.CalculateTravelTime(position,
+                    speedCautious: false,
+                    sleepModeInn: false,
+                    travelShip: false,
+                    hasHorse: horse,
+                    hasCart: cart);
+                travelTimeTotalMins = GameManager.Instance.GuildManager.FastTravel(travelTimeTotalMins);
+
+                //TODO: can make this calc dynamic based on mod settings
+                float travelTimeMinsMult = 0.8f * 2;
+                travelTimeTotalMins = (int)(travelTimeTotalMins / travelTimeMinsMult);
+                //int travelTimeHours = travelTimeTotalMins / 60;
+                //int travelTimeMinutes = travelTimeTotalMins % 60;
+
+                //return string.Format("{0} hours {1} mins travel", travelTimeHours, travelTimeMinutes);
+
+                return ConvertMinutesToDays(travelTimeTotalMins);
+
+                //return string.Format("{0} hours {1} mins travel", travelTimeHours, travelTimeMinutes);
+            }
+            else
+            {
+                bool hasHorse = GameManager.Instance.TransportManager.HasHorse();
+                bool hasCart = GameManager.Instance.TransportManager.HasCart();
+
+                //TODO: can consider ship calcs
+                //bool hasShip = GameManager.Instance.TransportManager.ShipAvailiable();
+
+                var travelTimeCalculator = new TravelTimeCalculator();
+
+                var travelTimeTotalMins = travelTimeCalculator.CalculateTravelTime(position,
+                    speedCautious: true,
+                    sleepModeInn: true,
+                    travelShip: false,
+                    hasHorse: hasHorse,
+                    hasCart: hasCart);
+
+                // Players can have fast travel benefit from guild memberships
+                travelTimeTotalMins = GameManager.Instance.GuildManager.FastTravel(travelTimeTotalMins);
+
+                int travelTimeDaysTotal = (travelTimeTotalMins / 1440);
+
+                // Classic always adds 1. For DF Unity, only add 1 if there is a remainder to round up.
+                if ((travelTimeTotalMins % 1440) > 0)
+                    travelTimeDaysTotal += 1;
+
+        //        return $"{travelTimeDaysTotal} days travel";
+        //    }
+        //}
+                return travelTimeDaysTotal;
+            }
+        }
+
+        public static string GetTravelTimeToLocationFromTravelOptions(DFLocation location)
+        {
+            DFPosition position = MapsFile.LongitudeLatitudeToMapPixel(location.MapTableData.Longitude, location.MapTableData.Latitude);
+
+            TransportManager transportManager = GameManager.Instance.TransportManager;
+            bool horse = transportManager.TransportMode == TransportModes.Horse;
+            bool cart = transportManager.TransportMode == TransportModes.Cart;
 
             var travelTimeCalculator = new TravelTimeCalculator();
 
             var travelTimeTotalMins = travelTimeCalculator.CalculateTravelTime(position,
-                speedCautious: true,
-                sleepModeInn: true,
+                speedCautious: false,
+                sleepModeInn: false,
                 travelShip: false,
-                hasHorse: hasHorse,
-                hasCart: hasCart);
-
-            // Players can have fast travel benefit from guild memberships
+                hasHorse: horse,
+                hasCart: cart);
             travelTimeTotalMins = GameManager.Instance.GuildManager.FastTravel(travelTimeTotalMins);
 
-            int travelTimeDaysTotal = (travelTimeTotalMins / 1440);
+            //TODO: can make this calc dynamic based on mod settings
+            float travelTimeMinsMult = 0.8f * 2;
+            travelTimeTotalMins = (int)(travelTimeTotalMins / travelTimeMinsMult);
+            int travelTimeHours = travelTimeTotalMins / 60;
+            int travelTimeMinutes = travelTimeTotalMins % 60;
 
-            // Classic always adds 1. For DF Unity, only add 1 if there is a remainder to round up.
-            if ((travelTimeTotalMins % 1440) > 0)
-                travelTimeDaysTotal += 1;
-
-            return $"{travelTimeDaysTotal} days travel";
-            
+            return string.Format("{0} hours {1} mins travel", travelTimeHours, travelTimeMinutes);            
         }
     }
 }
