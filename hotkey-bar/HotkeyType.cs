7b204b90e3481bf4e1225aba849a119c887c7558
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-08-10 23:42:20 +0100

    Update HotkeyType.cs

--- hotkey-bar/HotkeyType.cs
+++ hotkey-bar/HotkeyType.cs
@@ -4,6 +4,19 @@ namespace Assets.Scripts.Game.MacadaynuMods.HotkeyBar
     {
         Weapon = 0,
         Potion = 1,
-        Spell = 2
+        Spell = 2,
+        Horse = 3,
+        Cart = 4,
+        EnchantedItem = 5,
+        LightSource = 6,
+        UsableUselessItem2 = 7,
+        GenericItem = 8,
+        Book = 9,
+        Map = 10,
+        Spellbook = 11,
+        Drug = 12,
+        Oil = 13,
+        CampingEquipment = 14,
+        Meat = 15
     }
 }
