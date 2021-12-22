448d5d3d6a8c122c6f3ca46e467ae4f20bdd8a1b
Macadaynu <59894472+Macadaynu@users.noreply.github.com>
2021-12-22 14:19:06 +0000

    Add files via upload

---
+++ hotkey-bar/SpellIconData.cs
@@ -0,0 +1,12 @@
+using DaggerfallWorkshop.Game.MagicAndEffects;
+
+namespace Assets.MacadaynuMods.HotkeyBar
+{
+    public class SpellIconData
+    {
+        public int? StandardSpellIndex { get; set; }
+        public int IconIndex { get; set; }
+        public SpellIcon SpellIcon { get; set; }
+        public string SpellName { get; set; }
+    }
+}
